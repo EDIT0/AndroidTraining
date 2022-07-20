1ee1110d68040ea4e42049379ae8680a886af11b
edit0 <akdmadl34@naver.com>
2022-07-20 22:07:51 +0900

    AsynchronousAndSequentialExecution

--- CoroutineStudy/app/src/main/java/com/example/coroutinestudy/AsynchronousAndSequentialExecutionActivity.kt
+++ CoroutineStudy/app/src/main/java/com/example/coroutinestudy/AsynchronousAndSequentialExecutionActivity.kt
@@ -25,7 +25,11 @@ class AsynchronousAndSequentialExecutionActivity : AppCompatActivity() {
     val BASE_URL = "https://api.themoviedb.org/3/"
     val API_KEY = ""
 
-    val popularMovies: MutableLiveData<Response<MovieModel>> = MutableLiveData()
+    val popularMoviesPage1: MutableLiveData<Response<MovieModel>> = MutableLiveData()
+    val popularMoviesPage2: MutableLiveData<Response<MovieModel>> = MutableLiveData()
+    val popularMoviesPage3: MutableLiveData<Response<MovieModel>> = MutableLiveData()
+    val popularMoviesPage4: MutableLiveData<Response<MovieModel>> = MutableLiveData()
+    val popularMoviesPage5: MutableLiveData<Response<MovieModel>> = MutableLiveData()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -52,33 +56,50 @@ class AsynchronousAndSequentialExecutionActivity : AppCompatActivity() {
             val a = retrofitApi.getPopularMovies(API_KEY, "ko-KR", 1)
             Log.i("MYTAG", "page 1 execute!! ${Thread.currentThread().name}")
             withContext(Dispatchers.Main) {
-                popularMovies.setValue(a)
+                popularMoviesPage1.setValue(a)
             }
             val b = retrofitApi.getPopularMovies(API_KEY, "ko-KR", a.body()?.page!! + 1)
             Log.i("MYTAG", "page 2 execute!! ${Thread.currentThread().name}")
             withContext(Dispatchers.Main) {
-                popularMovies.setValue(b)
+                popularMoviesPage2.setValue(b)
             }
             val c = retrofitApi.getPopularMovies(API_KEY, "ko-KR", b.body()?.page!! + 1)
             Log.i("MYTAG", "page 3 execute!! ${Thread.currentThread().name}")
             withContext(Dispatchers.Main) {
-                popularMovies.setValue(c)
+                popularMoviesPage3.setValue(c)
             }
             val d = retrofitApi.getPopularMovies(API_KEY, "ko-KR", c.body()?.page!! + 1)
             Log.i("MYTAG", "page 4 execute!! ${Thread.currentThread().name}")
             withContext(Dispatchers.Main) {
-                popularMovies.setValue(d)
+                popularMoviesPage4.setValue(d)
             }
             val e = retrofitApi.getPopularMovies(API_KEY, "ko-KR", d.body()?.page!! + 1)
             Log.i("MYTAG", "page 5 execute!! ${Thread.currentThread().name}")
             withContext(Dispatchers.Main) {
-                popularMovies.setValue(e)
+                popularMoviesPage5.setValue(e)
             }
 
         }
 
-        popularMovies.observe(this as LifecycleOwner, {
+        popularMoviesPage1.observe(this as LifecycleOwner, {
             Log.i("MYTAG", "${Thread.currentThread().name} / page is ${it.body()?.page} / ${it.body().toString()}")
         })
+
+        popularMoviesPage2.observe(this as LifecycleOwner, {
+            Log.i("MYTAG", "${Thread.currentThread().name} / page is ${it.body()?.page} / ${it.body().toString()}")
+        })
+
+        popularMoviesPage3.observe(this as LifecycleOwner, {
+            Log.i("MYTAG", "${Thread.currentThread().name} / page is ${it.body()?.page} / ${it.body().toString()}")
+        })
+
+        popularMoviesPage4.observe(this as LifecycleOwner, {
+            Log.i("MYTAG", "${Thread.currentThread().name} / page is ${it.body()?.page} / ${it.body().toString()}")
+        })
+
+        popularMoviesPage5.observe(this as LifecycleOwner, {
+            Log.i("MYTAG", "${Thread.currentThread().name} / page is ${it.body()?.page} / ${it.body().toString()}")
+        })
+
     }
 }
