d26c2b3a1ef923e4a0f7e5baca9316ba1e43fb89
edit0 <akdmadl34@naver.com>
2022-11-08 23:40:02 +0900

    mvc2

--- MVCExample1/app/src/main/java/com/example/mvcexample1/controller/MainActivity.kt
+++ MVCExample1/app/src/main/java/com/example/mvcexample1/controller/MainActivity.kt
@@ -1,38 +1,158 @@
 package com.example.mvcexample1.controller
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
-import com.example.mvcexample1.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout.OnRefreshListener
+import com.example.mvcexample1.controller.adapter.MovieAdapter
 import com.example.mvcexample1.databinding.ActivityMainBinding
 import com.example.mvcexample1.model.Movie
 import com.example.mvcexample1.network.ApiService
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.lang.String
 import javax.inject.Inject
+import kotlin.Int
+import kotlin.apply
+import kotlin.properties.Delegates
+import kotlin.reflect.KProperty
+
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+    interface movieCallback {
+        suspend fun isLoading(value: Boolean)
+    }
+
     private val TAG = MainActivity::class.java.simpleName
 
-    private lateinit var binding : ActivityMainBinding
-
+    private lateinit var binding: ActivityMainBinding
     @Inject
+    lateinit var apiService: ApiService
     lateinit var movie: Movie
+    private lateinit var movieAdapter: MovieAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        movie = Movie(apiService, object : MainActivity.movieCallback {
+            override suspend fun isLoading(value: Boolean) {
+                withContext(Dispatchers.Main) {
+                    binding.swipeRefresh.isRefreshing = value
+                }
+            }
+        })
+
+        movieAdapter = MovieAdapter()
+
+        binding.rvMovie.apply {
+            adapter = movieAdapter
+            layoutManager = LinearLayoutManager(binding.root.context)
+            addOnScrollListener(onScrollListener)
+        }
+
         binding.btnSearch.setOnClickListener {
             lifecycleScope.launch(Dispatchers.IO) {
-                Log.i(TAG, "${movie.searchMovies(binding.etInput.text.toString().trim())}")
+                movie.setSearchQuery(binding.etInput.text.toString().trim())
+                movie.setPage(1)
+                movieAdapter.submitList(emptyList())
+
+                val result = movie.searchMovies()
+                withContext(Dispatchers.Main) {
+                    movieAdapter.apply {
+                        submitList(result) {
+                            notifyDataSetChanged()
+                        }
+                    }
+                }
             }
         }
 
+        movieAdapter.setOnItemClickListener { i, movieModelResult ->
+            Toast.makeText(binding.root.context, "${i} / ${movieModelResult.title}", Toast.LENGTH_SHORT).show()
+        }
+
+        binding.swipeRefresh.setOnRefreshListener {
+            lifecycleScope.launch(Dispatchers.IO) {
+                movie.setSearchQuery(binding.etInput.text.toString().trim())
+                movie.setPage(1)
+                movieAdapter.submitList(emptyList())
+
+                val result = movie.searchMovies()
+                withContext(Dispatchers.Main) {
+                    movieAdapter.apply {
+                        submitList(result) {
+                            notifyDataSetChanged()
+                            binding.swipeRefresh.isRefreshing = false
+                        }
+                    }
+                }
+            }
+        }
+
+        binding.btnRefresh.setOnClickListener {
+            lifecycleScope.launch(Dispatchers.IO) {
+                movie.setSearchQuery(binding.etInput.text.toString().trim())
+                movie.setPage(1)
+                movieAdapter.submitList(emptyList())
+
+                val result = movie.searchMovies()
+                withContext(Dispatchers.Main) {
+                    movieAdapter.apply {
+                        submitList(result) {
+                            notifyDataSetChanged()
+                            binding.swipeRefresh.isRefreshing = false
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    private val onScrollListener = object : RecyclerView.OnScrollListener() {
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+
+            val lastVisibleItemPosition =
+                (recyclerView.layoutManager as LinearLayoutManager?)!!.findLastCompletelyVisibleItemPosition() // 화면에 보이는 마지막 아이템의 position
+            val itemTotalCount = recyclerView.adapter!!.itemCount - 1 // 어댑터에 등록된 아이템의 총 개수 -1
+
+            // 스크롤이 끝에 도달했는지 확인
+            movie.apply {
+                if (lastVisibleItemPosition == itemTotalCount) {
+                    if (getLoadingState()) {
+
+                    } else {
+                        if (getTotalPages() != getCurrentPage()) {
+                            lifecycleScope.launch(Dispatchers.IO) {
+                                Log.i(TAG, "TotalPages = ${getTotalPages()}")
+                                val result = searchMovies()
+                                withContext(Dispatchers.Main) {
+                                    movieAdapter.apply {
+                                        submitList(result) {
+                                            notifyDataSetChanged()
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+        }
     }
 }
--- MVCExample1/app/src/main/java/com/example/mvcexample1/model/Movie.kt
+++ MVCExample1/app/src/main/java/com/example/mvcexample1/model/Movie.kt
@@ -3,15 +3,18 @@ package com.example.mvcexample1.model
 import android.app.Activity
 import android.util.Log
 import com.example.mvcexample1.BuildConfig
+import com.example.mvcexample1.controller.MainActivity
 import com.example.mvcexample1.model.data.MovieModel
 import com.example.mvcexample1.network.ApiService
 import com.example.mvcexample1.util.ERROR
 import com.example.mvcexample1.util.NO_DATA
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.*
 import javax.inject.Inject
 
 class Movie(
-    private val apiService: ApiService
+    private val apiService: ApiService,
+    private val movieCallback: MainActivity.movieCallback
 ) {
 
     private val TAG = Movie::class.java.simpleName
@@ -20,14 +23,28 @@ class Movie(
 
     private var movieList = ArrayList<MovieModel.MovieModelResult>()
 
-    suspend fun searchMovies(name: String) : List<MovieModel.MovieModelResult> {
+    private var isLoading = false
+    private var totalPages = 0
+    private var page = 0
+
+    fun setSearchQuery(query: String) {
+        currentSearchQuery = query
+    }
+    fun getLoadingState() : Boolean = isLoading
+    fun getTotalPages() : Int = totalPages
+    fun getCurrentPage() : Int = page
+    fun setPage(page : Int) {
+        this.page = page
+    }
+
+    suspend fun searchMovies() : List<MovieModel.MovieModelResult> {
         try {
             val result = flow<MovieModel> {
                 val response = apiService.getSearchMovies(
                     BuildConfig.API_KEY,
-                    name,
+                    currentSearchQuery,
                     "en_US",
-                    1
+                    page
                 )
 
                 if(response.isSuccessful) {
@@ -35,6 +52,7 @@ class Movie(
                         throw Exception(NO_DATA)
                     } else {
                         response.body()?.let {
+                            delay(1000L)
                             emit(it)
                         }
                     }
@@ -46,16 +64,28 @@ class Movie(
             result
                 .onStart {
                     Log.i(TAG, "onStart()")
+                    isLoading = true
+                    movieCallback.isLoading(isLoading)
                 }
                 .onCompletion {
                     Log.i(TAG, "onCompletion()")
+                    isLoading = false
+                    movieCallback.isLoading(isLoading)
                 }
                 .catch {
                     Log.i(TAG, "catch : ${it.message}")
+                    throw Exception(it.message)
                 }
                 .collect {
                     Log.i(TAG, "${it}")
-                    movieList = it.movieModelResults as ArrayList
+                    if(page == 1) {
+                        Log.i(TAG, "Clear Movie List")
+                        movieList.clear()
+                    }
+                    totalPages = it.totalPages
+                    var tempArray = it.movieModelResults as ArrayList
+                    movieList.addAll(tempArray)
+                    page++
                 }
 
             return movieList
