a50cc861070f20a786a1961ee122e8ec1bd08a6d
EDIT0 <akdmadl34@naver.com>
2024-05-21 11:17:19 +0900

    SkeletonLoading EditText + SharedFlow debounce

--- SkeletonLoading/app/src/main/java/com/edit/skeletonloading/MainActivity.kt
+++ SkeletonLoading/app/src/main/java/com/edit/skeletonloading/MainActivity.kt
@@ -2,12 +2,24 @@ package com.edit.skeletonloading
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
 import android.view.View
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.edit.skeletonloading.databinding.ActivityMainBinding
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -15,6 +27,8 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var adapter = Adapter()
     private var list = ArrayList<Model>()
+
+    private var sharedFlow = MutableSharedFlow<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -43,11 +57,44 @@ class MainActivity : AppCompatActivity() {
 
         CoroutineScope(Dispatchers.Main).launch {
             showSampleData(isLoading = true)
-            delay(5000L)
+            delay(1000L)
             adapter.submitList(list.toMutableList())
             showSampleData(isLoading = false)
         }
 
+
+        binding.et.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                Log.d("MYTAG", "텍스트 변경: ${p0}")
+                lifecycleScope.launch {
+                    sharedFlow.emit(p0.toString())
+                }
+            }
+
+        })
+
+        lifecycleScope.launch {
+            sharedFlow
+                .distinctUntilChanged()
+                .debounce(1000)
+//                .filter {
+//                    it != ""
+//                }
+                .onEach {
+                    Log.d("MYTAG", "검색 ${it}")
+                }
+                .collect {
+                    Log.d("MYTAG", "검색 Collect ${it}")
+                }
+        }
     }
 
     private fun showSampleData(isLoading: Boolean) {
