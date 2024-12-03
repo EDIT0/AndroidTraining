f1d57299bed9a783865cd1c63d17a762d7992a68
EDIT0 <akdmadl34@naver.com>
2024-12-03 10:03:08 +0900

    StatusbarControlDemo1 Q(30) 버전 대응 코드 업데이트

--- StatusbarControlDemo1/app/src/main/java/com/example/statusbarcontroldemo1/Util.kt
+++ StatusbarControlDemo1/app/src/main/java/com/example/statusbarcontroldemo1/Util.kt
@@ -9,6 +9,7 @@ import android.util.TypedValue
 import android.view.View
 import android.view.WindowManager
 import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsControllerCompat
 
 object Util {
     fun Activity.setStatusBarTransparent() {
@@ -71,5 +72,98 @@ object Util {
         val resources: Resources = context.resources
         val metrics: DisplayMetrics = resources.getDisplayMetrics()
         return (px / (metrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)).toInt()
+    }
+
+    /**
+     * 상단 상태바, 하단 내비게이션바 설정
+     * 상단 상태바는 영역 사용 O, 하단 내비게이션바는 영역 사용은 isNaviBlock 값에 따라 유동적
+     *
+     * @param activity
+     * @param view
+     * @param isNaviBlock 하단 내비게이션바 true: show, false: hide
+     */
+    fun setStatusNavigationBar(activity: Activity, isNaviBlock: Boolean) {
+        // Status, Navigation Bar 열기
+        if(isNaviBlock) {
+            activity.apply {
+                window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+                window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+                window.statusBarColor = 0x00000000  // transparent
+
+                window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            }
+        } else {
+            activity.window.apply {
+                setFlags(
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+                )
+            }
+        }
+
+        if (Build.VERSION.SDK_INT >= 30) {    // API 30 에 적용
+            WindowCompat.setDecorFitsSystemWindows(activity.window, false)
+        }
+
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+//            activity.window.decorView.setOnApplyWindowInsetsListener { view, insets ->
+//                val systemBarsInsets = insets.systemWindowInsets
+//                view.setPadding(0, 0, 0, systemBarsInsets.bottom)
+//                insets
+//            }
+//        } else {
+//            activity.window.decorView.setOnApplyWindowInsetsListener { view, insets ->
+//                // 상태 바와 내비게이션 바의 인셋을 가져옵니다.
+//                view.setPadding(0, 0, 0, activity.navigationHeight())
+//                insets
+//            }
+//        }
+    }
+
+    /**
+     * 상단 상태바 설정 (Padding, Icon color)
+     *
+     * @param activity
+     * @param view
+     * @param isStatusBarPadding 상태바 사용 true, 사용하지 않음 false
+     * @param isNavigationBarPadding
+     * @param isStatusIconBlack 상태바 아이콘 ture: Black
+     * @param isNaviIconBlack
+     */
+    fun setStatusNaviBarPadding(
+        activity: Activity,
+        view: View,
+        isStatusBarPadding: Boolean,
+        isNavigationBarPadding: Boolean,
+        isStatusIconBlack: Boolean,
+        isNaviIconBlack: Boolean
+    ) {
+        // 상태바 아이콘 색상
+        val wic = WindowInsetsControllerCompat(activity.window, activity.window.decorView)
+        wic.isAppearanceLightStatusBars = isStatusIconBlack // true: 어두운 아이콘, false: 밝은 아이콘
+        wic.isAppearanceLightNavigationBars = isNaviIconBlack // true: 어두운 아이콘, false: 밝은 아이콘
+
+        if (Build.VERSION.SDK_INT >= 30) {
+            if(isStatusBarPadding && isNavigationBarPadding) {
+                view.setPadding(0, activity.statusBarHeight(), 0, activity.navigationHeight())
+            } else if(isStatusBarPadding) {
+                view.setPadding(0, activity.statusBarHeight(), 0, 0)
+            } else if(isNavigationBarPadding) {
+                view.setPadding(0, 0, 0, activity.navigationHeight())
+            } else {
+                view.setPadding(0, 0, 0, 0)
+            }
+        } else {
+            if(isStatusBarPadding && isNavigationBarPadding) {
+                view.setPadding(0, activity.statusBarHeight(), 0, 0)
+            } else if(isStatusBarPadding) {
+                view.setPadding(0, activity.statusBarHeight(), 0, 0)
+            } else if(isNavigationBarPadding) {
+                view.setPadding(0, 0, 0, 0)
+            } else {
+                view.setPadding(0, 0, 0, 0)
+            }
+        }
+
     }
 }
